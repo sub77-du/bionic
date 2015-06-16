732e863a5643f9675c572e478ead1947c9c7e023
Dan Albert <danalbert@google.com>
2015-06-16 13:57:54 -0700

    Use new sanitizer option.
    Change-Id: I21b04a1f3be3b10ee557bf9e0caa5db638686364

--- libm/Android.mk
+++ libm/Android.mk
@@ -504,7 +504,7 @@ LOCAL_CONLY_FLAGS := \
     -std=c99 \
 
 LOCAL_NATIVE_COVERAGE := $(bionic_coverage)
-LOCAL_ADDRESS_SANITIZER := false
+LOCAL_SANITIZE := never
 include $(BUILD_STATIC_LIBRARY)
 
 # -----------------------------------------------------------------------------
@@ -526,7 +526,7 @@ LOCAL_SYSTEM_SHARED_LIBRARIES := libc
 LOCAL_WHOLE_STATIC_LIBRARIES := libm
 
 LOCAL_NATIVE_COVERAGE := $(bionic_coverage)
-LOCAL_ADDRESS_SANITIZER := false
+LOCAL_SANITIZE := never
 
 LOCAL_CXX_STL := none
 
